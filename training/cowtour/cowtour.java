2db53e7d00b7e98cafce256bbe1c707eb0bb3dd8
John Hunter Kohler <jhunterkohler@gmail.com>
2020-09-16 16:44:22 -0400

    working on fracdec

--- training/cowtour/cowtour.java
+++ training/cowtour/cowtour.java
@@ -7,59 +7,96 @@ class cowtour {
         BufferedReader br = new BufferedReader(new FileReader("./cowtour.in"));
 
         int n = Integer.parseInt(br.readLine());
-        double[][] adj = new double[n][n];
-        double[][] locations = new double[n][2];
+        double[][] loc = double[n][2];
+        double[][] planeDist = double[n][n];
+        boolean[][] adj = new boolean[n][n];
+        List<List<Integer>> parts = new ArrayList<List<Integer>>();
 
-        for(int i = 0; i < n;i++) {
+        for(int i = 0; i < n; i++) {
             String[] line = br.readLine().split(" ");
-            locations[i][0] = Double.parseDouble(line[0]);
-            locations[i][1] = Double.parseDouble(line[1]);
+            loc[0] = Integer.parseInt(line[0]);
+            loc[1] = Integer.parseInt(line[1]);
         }
 
-        for(int j = 0; j < n; j++) {
+        for(int j = 0; j < n;j++) {
             String[] line = br.readLine().split("");
             for(int i = 0; i < n;i++) {
-                adj[i][j] = Double.parseDouble(line[i]);
+                adj[i][j] = line[i].equals("1");
+                planedist[i][j] = distance(loc[i], loc[j]);
             }
         }
 
-        double[][] dist = floyd(adj);
-        
+        List<List> parts = partition(adj);
+        double[][] graphDist = floyd(adj, planeDist);
+
+        List<Integer> diameters = new ArrayList<Integer>();
+        for(List part: parts) {
+            int max = 0;
+
+            for(int i: part) {
+                for(int j: part){
+                    max = Math.max(max, graphDist[i][j])
+                }
+            }
+
+            diameters.add(max);
+        }
+
     }
 
-    static double pointDist(double x0, double y0, double x1, double y1) {
-        return Math.sqrt(Math.pow(x0-x1, 2) + Math.pow(y0-y1, 2));
+    static List<List> partition(boolean[][]) {
+        boolean[] visited = new boolean[];
+        List<List> parts = new List<List>();
+        for(int i = 0; i < n; i++) {
+            if(visited[i]) { continue; }
+            List<Integer> part = new ArrayList<Integer>();
+            Queue<Integer> q = new Queue<Integer>();
+
+
+            while(!q.isEmpty()) {
+                int head = q.pop();
+                if(visited[head]) { continue; }
+                visited[head] = true;
+
+                for(int j = 0; j < n;j++) {
+                    if(adj[head][j]) {
+                        queue.add(j);
+                    }
+                }
+            }
+        }
     }
 
-    static double pointDist(double[] p, double q[]) {
-        return pointDist(p[0], p[1], q[0], q[1]);
+    static double distance(double[] p, double[] q) {
+        return Math.sqrt(Math.pow(p[0] - q[0], 2) + Math.pow(p[1] - p[0], 2));
     }
 
-    static double[][] floyd(double[][] adj) {
+    static double[][] floyd(boolean[][] adj, double[][] planeDist) {
         int n = adj.length;
-        double[][] dist = new double[n][n];
-        for(int j = 0; j < n; j++) {
-            for(int i = 0; i < n;i++) {
+        double[][] graphDist = new double[][];
+
+        for(int i = 0; i < n;i++) {
+            for(int j = 0; j < n;j++) {
                 if(i == j) {
-                    dist[i][j] = 0;
+                    graphDist[i][j] = 0;
+                } else if(adj[i][j]) {
+                    graphDist[i][j] = planeDist[i][j]
                 } else {
-                    dist[i][j] = adj[i][j] == 1
-                        ? pointDist(locations[i], locations[j])
-                        : Integer.MAX_VALUE;
+                    graphDist[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i =0 ; i < n;i++) {
+            for(int j =0 ; j < n;j++) {
                 for(int k = 0; k < n;k++) {
-                    if(dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE ) {
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    if( graphDist[i][k] != Integer.MAX_VALUE && graphDist[k][j] != Integer.MAX_VALUE ) {
+                        graphDist[i][j] = Math.max(graphDist[i][j], graphDist[i][k] + graphDist[k][j]);
                     }
                 }
             }
         }
 
-        return dist;
+        return graphDist;
     }
 }
--- training/fracdec/fracdec.java
+++ training/fracdec/fracdec.java
@@ -1,14 +1,82 @@
+/*
+ID: jhunter3
+LANG: JAVA
+PROB: fracdec
+*/
+
 import java.io.*;
 import java.util.*;
 import java.math.*;
 
 class fracdec {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(new File('./fracdec.in'));
-        double n = sc.nextInt();
-        double d = sc.nextInt();
+        long t = System.nanoTime();
+        Scanner sc = new Scanner(new File("./fracdec.in"));
+        int n = sc.nextInt();
+        int d = sc.nextInt();
 
-        System.out.println(n / d);
+        String[] str = ( n > d
+            ? stringify(n,d).split("\\.")[0] + "." + stringify(n % d, d).split("\\.")[1]
+            : stringify(n,d) )
+                .split("(?<=\\G.{76})");
+
+        PrintWriter pw = new PrintWriter(new File("./fracdec.out"));
+        for(String line: str) {
+            System.out.println(line);
+            pw.println(line);
+        }
+        pw.close();
+    }
+
+    static String stringify(int n, int d) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        int num = n;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            if(num == 0) { break; }
+
+            boolean first = true;
+            while(true) {
+                if(num < d) {
+                    num = num * 10;
+                    if(!first) {
+                        sb.append(0);
+                    }
+                    first = false;
+                } else {
+                    break;
+                }
+            }
+
+            System.out.println(map.toString());
+
+            if(map.keySet().contains(num)) {
+                sb.insert((int) map.get(num),'(');
+                sb.append(')');
+                break;
+            }
+
+            int q = num / d;
+            int r = num % d;
+            map.put(num, sb.length());
+
+            sb.append(q);
+            num = r;
+        }
+
+        String calc = String.valueOf((((double) n) / ((double) d)));
+        int shift = calc.indexOf(".") - (calc.charAt(0) == '0' ? 1 : 0);
+        sb.insert(shift, '.');
+        if(sb.charAt(0) == '.') {
+            sb.insert(0,0);
+        }
+        if(sb.charAt(sb.length() - 1) == '.') {
+            sb.append(0);
+        }
+
+        return sb.toString();
     }
 
 
