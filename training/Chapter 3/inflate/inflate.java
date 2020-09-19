17a368cc46c3540a68828222ec1c225cbb3c17dc
John H. Kohler <jhunterkohler@gmail.com>
2020-09-19 19:15:41 -0400

    working on contact

--- training/Chapter 3/inflate/inflate.java
+++ training/Chapter 3/inflate/inflate.java
@@ -9,66 +9,77 @@ import java.util.*;
 import java.math.*;
 
 class inflate {
+    static int m, n;
+    static Problem[] problems;
     public static void main(String[] args) throws IOException {
         long t = System.nanoTime();
 
-        StringTokenizer st;
-        BufferedReader br;
-        BufferedWriter bw;
-
-        br = new BufferedReader(new FileReader("./inflate.in"));
-        st = new StringTokenizer(br.readLine());
-
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        PriorityQueue<Problem> pq =
-            new PriorityQueue<Problem>((Problem p, Problem q) -> {
-                return p.density > q.density ? -1 : 1;
-            });
-
+        Scanner sc = new Scanner(new File("./inflate.in"));
+        m = sc.nextInt();
+        n = sc.nextInt();
+        problems = new Problem[n];
         for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            pq.add(new Problem(st.nextToken(), st.nextToken(), i + 1));
+            problems[i] = new Problem(sc.nextInt(), sc.nextInt());
         }
 
-        // System.out.println(pq);
+        // System.out.println("m: " + m);
+        // System.out.println("n: " + n);
+        // System.out.println("problems: " + Arrays.toString(problems));
+
+        int ans =  solve(m,n,problems);
+
+        // System.out.println("ans: " + ans);
+        BufferedWriter bw = new BufferedWriter(new FileWriter("./inflate.out"));
+        bw.write(ans + "\n");
+        bw.close();
+        System.out.println("time: " + ((System.nanoTime() - t) / Math.pow(10,9)));
+    }
+
+    static Map<List<Integer>, Integer> mem;
+    static int solve(int m, int n, Problem[] problems) {
+        mem = new HashMap<List<Integer>,Integer>();
+        return maximize(m, 0);
+    }
+
+    static int maximize(int time, int sum) {
+        if(mem.containsKey(zip(time,sum))) {
+            return mem.get(zip(time, sum));
+        }
+        if(time < 0) {
+            return 0;
+        } else if(time == 0) {
+            return sum;
+        }
 
         int max = 0;
-        while(!pq.isEmpty()) {
-            Problem head = pq.peek();
-            if(m >= head.minutes) {
-                m -= head.minutes;
-                max += head.points;
-                System.out.println(head.i);
-            } else {
-                pq.poll();
-            }
-        }
-        //
-        System.out.println("max: " + max);
-        System.out.println("time: " + ((System.nanoTime() - t) / Math.pow(10,9)));
+        for(Problem p: problems){
+            int newtime = time - p.len;
+            int newsum = sum + p.val;
 
-        bw = new BufferedWriter(new FileWriter("./inflate.out"));
-        bw.write(max + "\n");
-        bw.close();
+            max = Math.max(max, max + maximize(newtime, newsum));
+        }
+
+        mem.put(zip(time, sum), max);
+        return max;
+    }
+
+    static List<Integer> zip(int a, int b) {
+        return Arrays.asList(new Integer[]{a,b});
     }
 
     static class Problem {
-        int points, minutes, i;
-        double density;
-        Problem(int points, int minutes, int i) {
-            this.points = points;
-            this.minutes = minutes;
-            this.density = ((double) this.points) / ((double) this.minutes);
-            this.i = i;
+        int val,len;
+        Problem(int val, int len) {
+            this.val = val;
+            this.len = len;
         }
 
-        Problem(String points, String minutes, int i){
-            this(Integer.parseInt(points), Integer.parseInt(minutes), i);
+        Problem(String val, String len) {
+            this(Integer.parseInt(val), Integer.parseInt(len));
         }
 
         public String toString() {
-            return String.format("%.2f", this.density);
+            return("{val: " + this.val + ", len: " + this.len + "}");
         }
     }
 }
