77de87f16b772b73dbad5fbee3080681e61bf49c
John H. Kohler <jhunterkohler@gmail.com>
2020-09-19 23:15:27 -0400

    done with stamps

--- training/Chapter 3/stamps/stamps.java
+++ training/Chapter 3/stamps/stamps.java
@@ -1,67 +1,56 @@
+/*
+ID: jhunter3
+LANG: JAVA
+PROB: stamps
+*/
+
 import java.util.*;
 import java.io.*;
 
 class stamps {
-    static final int MAX_VALUE = 200 * 10000;
-
+    static int[] stamps;
     static int k, n;
-    static int[] bases;
-    static Set<List> mem;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("./stamps.in"));
         StringTokenizer st = new StringTokenizer(br.readLine());
         k = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        bases = new int[n];
-        mem = new HashSet<List>();
-
         String line;
+        stamps = new int[n];
+        int i = 0;
         while((line = br.readLine()) != null) {
             st = new StringTokenizer(line);
-            for(int i = 0 ; st.hasMoreTokens(); i++) {
-                bases[i] = Integer.parseInt(st.nextToken());
-                reached(bases[i], 1);
+            while(st.hasMoreTokens()){
+                stamps[i] = Integer.parseInt(st.nextToken());
+                i++;
             }
         }
-        reached(0, k);
-
-        int c = 1;
-        while(reachable(c, k))
-            c++;
-
-        for(List l: mem) {
-            System.out.printf("[%d,%d]\n", l.get(0), l.get(1));
-        }
-
-        System.out.println(c - 1);
+        int j = 1;
+        while(minstamps(j) <= k)
+            j++;
+        j--;
+        BufferedWriter bw = new BufferedWriter(new FileWriter("./stamps.out"));
+        bw.write(j + "\n");
+        bw.close();
     }
+    static short[] mem = new short[10000 * 200 + 1];
+    static {
+        Arrays.fill(mem, (short) -1);
+        mem[0] = 0;
+    }
+    static short minstamps(int j) {
+        if(mem[j] == -1){
+            mem[j] = Short.MAX_VALUE;
+            for(int stamp: stamps) {
+                if(j - stamp < 0)
+                    continue;
 
-    static boolean reachable(int val, int num) {
-        if(hasBeenReached(val,num)) {
-            return true;
-        } else {
-            for(int i: bases) {
-                if(val - i >= 0 && reachable(val - i, num - 1)) {
-                    return true;
+                short call = minstamps(j - stamp);
+                if(call != -1) {
+                    mem[j] = (short) Math.min(mem[j], call + 1);
                 }
             }
         }
-
-        return false;
-    }
-
-    static void reached(int val, int num) {
-        while(num <= k) {
-            mem.add(zip(val, num));
-            num++;
-        }
-    }
-
-    static boolean hasBeenReached(int val, int num) {
-        return mem.contains(zip(val, num));
-    }
-
-    static List zip(int a, int b) {
-        return Arrays.asList(new Integer[]{a,b});
+        return mem[j];
     }
 }
