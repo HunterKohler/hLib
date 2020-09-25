b0b83066e09158e8eeb23758856e98e86cf6b176
John H. Kohler <jhunterkohler@gmail.com>
2020-09-25 18:10:33 -0400

    finished compilation

---
+++ library/src/library/util/Methods.java
@@ -0,0 +1,20 @@
+package library.util;
+import java.util.*;
+
+public class Methods {
+    // primitive zip is pzip as generics dont support prims
+    public static int[]         pzip(int ...args)       { return args; }
+    public static char[]        pzip(char ...args)      { return args; }
+    public static short[]       pzip(short ...args)     { return args; }
+    public static long[]        pzip(long ...args)      { return args; }
+    public static boolean[]     pzip(boolean ...args)   { return args; }
+    public static double[]      pzip(double ...args)    { return args; }
+    public static float[]       pzip(float ...args)     { return args; }
+    @SuppressWarnings("unchecked")
+    public static <T> T[]       zip(T ...args)          { return args; }
+
+    @SuppressWarnings("unchecked")
+    public static <T> List<T>   list(T ...args)         { return new ArrayList<T>(Arrays.asList(args)); }
+
+
+}
