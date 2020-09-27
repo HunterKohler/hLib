6c706b9b7d614b69a4090fcb2f5b4bb22a722c7b
John H. Kohler <jhunterkohler@gmail.com>
2020-09-27 01:21:41 -0400

    idk

--- training/Chapter 3/game1/game1.java
+++ training/Chapter 3/game1/game1.java
@@ -1,91 +1,81 @@
 import java.io.*;
 import java.util.*;
-
 import static java.lang.Math.*;
 import static java.lang.Integer.parseInt;
 
 class game1 {
     public static void main(String[] args) throws IOException {
-        long t = System.nanoTime();
-        BufferedReader br = new BufferedReader(new FileReader("game1.in"));
-
-        n = parseInt(br.readLine());
-        board = new int[n];
-
-        int i = 0;
+        BufferedReader br = new BufferedReader(new FileReader("./game1.in"));
+        int n = parseInt(br.readLine());
+        int[] board = new int[n];
         String line;
+        int index = 0;
         while((line = br.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(line);
             while(st.hasMoreTokens()) {
-                board[i] = parseInt(st.nextToken());
-                i++;
+                board[index] = parseInt( st.nextToken() );
+                index++;
             }
         }
 
-        System.out.println("n: " + n + "\nboard: " + Arrays.toString(board));
+        int[][][] mem = new int[n][n][2];
+        Set<List<Integer>> visited = new HashSet<List<Integer>>();
+        Queue<List<Integer>> q = new LinkedList<List<Integer>>();
+        for(int i = 0; i < n;i++) {
+            mem[i][i][n % 2] = 0;
+            mem[i][i][(n + 1) % 2] = board[i];
+            List<Integer> list = new ArrayList<Integer>(); // {i,j,p} -> {left, right, player}
+            list.add(i + 1);
+            list.add(i);
+            list.add(n % 2);
+            q.add(list);
 
-        int[] ans = optimal();
-        BufferedWriter bw = new BufferedWriter(new FileWriter("game1.out"));
-        bw.write(ans[0] + " " + ans[1] + "\n");
+            List<Integer> list2 = new ArrayList<Integer>(); // {i,j,p} -> {left, right, player}
+            list2.add(i);
+            list2.add(i + 1);
+            list2.add(n % 2);
+            q.add(list2);
+        }
+
+        while(!q.isEmpty()) {
+            List<Integer> current = q.poll();
+            int i = current.get(0);
+            int j = current.get(1);
+            int p = current.get(2);
+            if(!visited.contains(current) && i < n && j < n) {
+                if(i == 0) {
+                    mem[i][j] = mem[i][j - 1];
+                    mem[i][j][p] += board[j - 1];
+                } else if (j ==0 ) {
+                    mem[i][j] = mem[i - 1][j];
+                    mem[i][j][p] += board[i - 1];
+                } else if(mem[i][j - 1][p] > mem[i - 1][j][p]) {
+                    mem[i][j] = mem[i][j - 1];
+                    mem[i][j][p] += board[j - 1];
+                } else {
+                    mem[i][j] = mem[i - 1][j];
+                    mem[i][j][p] += board[i - 1];
+                }
+
+                visited.add(current);
+
+                List<Integer> list = new ArrayList<Integer>(); // {i,j,p} -> {left, right, player}
+                list.add(i + 1);
+                list.add(j);
+                list.add((p + 1) % 2);
+                q.add(list);
+
+                List<Integer> list2 = new ArrayList<Integer>(); // {i,j,p} -> {left, right, player}
+                list2.add(i);
+                list2.add(j + 1);
+                list2.add((p + 1) % 2);
+                q.add(list2);
+            }
+        }
+
+        System.out.println(Arrays.toString(mem[0][n - 1]));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("./game1.out"));
+        bw.write(mem[0][n - 1][0] + " " + mem[0][n - 1][1]);
         bw.close();
-
-        System.out.println("final: ");
-        printMem();
-        System.out.println("time: " + ((System.nanoTime() - t) / pow(10,9)));
-    }
-
-    static int n;
-    static int[] board;
-    static int[][][] mem; // left, right, player
-    static int[] optimal() {
-        mem = new int[n][n][2];
-        for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++) {
-            if(i == j){
-                mem[i][j][n % 2] = 0;
-                mem[i][j][flip(n % 2)] = board[i];
-            } else {
-                mem[i][j] = new int[] {-1,-1};
-            }
-        }
-
-        System.out.println("init: ");
-        printMem();
-
-        return optimal(0, n - 1, 0);
-    }
-
-    static int[] optimal(int left, int right, int player) { // player in {0,1}
-        System.out.printf("optimal(%d, %d, %d)\n", left, right,player);
-        printMem();
-
-        if(mem[left][right][player] == -1) {
-            int[] lOptimal = optimal(left + 1, right, flip(player));
-            lOptimal[player] += board[left];
-            int[] rOptimal = optimal(left, right - 1, flip(player));
-            rOptimal[player] += board[right];
-        }
-
-        if(mem[0][0][0] == 7) {
-            System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&");
-            printMem();
-            throw new Error("Whata");
-        }
-
-        return mem[left][right];
-    }
-
-    static int flip(int i) {
-        return (i + 1) % 2;
-    }
-
-    static void printMem() {
-        for(int[][] row: mem) {
-            System.out.print("    | ");
-            for(int[] arr: row) {
-                System.out.print(Arrays.toString(arr) + " ");
-            }
-            System.out.println("|");
-        }
     }
 }
