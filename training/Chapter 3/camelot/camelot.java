b1c57ee1707e97d4d39e14ec3900e02d682b48f8
John H. Kohler <jhunterkohler@gmail.com>
2020-09-28 13:47:47 -0400

    working on camelot

--- training/Chapter 3/camelot/camelot.java
+++ training/Chapter 3/camelot/camelot.java
@@ -10,18 +10,21 @@ import static java.lang.Math.*;
 import static java.lang.Integer.parseInt;
 
 class camelot {
+    static int R;
+    static int C;
+
     public static void main(String[] args) throws IOException {
         long t = System.nanoTime();
 
-        BufferedReader br = new BufferedReader(new FileReader());
+        BufferedReader br = new BufferedReader(new FileReader("./camelot.in"));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int R = parseInt(st.nextToken());
-        int C = parseInt(st.nextToken());
+        R = parseInt(st.nextToken());
+        C = parseInt(st.nextToken());
         List<Peice> peices = new ArrayList<Peice>();
         boolean first = true;
         String line;
         while((line = br.readLine()) != null) {
-            st = new StingTokenizer(line);
+            st = new StringTokenizer(line);
             if(first)
                 peices.add(new Peice(
                     st.nextToken().charAt(0) - 'A',
@@ -35,7 +38,7 @@ class camelot {
 
         Set<State> visited = new HashSet<State>();
         Queue<State> q = new LinkedList<State>();
-        q.add(new List<Integer>());
+        q.add(new State(peices));
 
         int minSoFar = Integer.MAX_VALUE;
         while(q.size() > 0) {
@@ -44,12 +47,16 @@ class camelot {
                 visited.add(current);
             } else if(current.moves.size() < minSoFar) {
                 if(peices.size() == 1) {
-                    minSoFar = peices.moves.size();
+                    minSoFar = current.moves.size();
                 } else {
                     q.addAll(current.next());
                 }
             }
         }
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("./camelot.out"));
+        bw.write(minSoFar);
+        bw.close();
 
         System.out.println("ans: " + minSoFar + " time: " + ((System.nanoTime() - t) / pow(10,9)));
     }
@@ -58,17 +65,25 @@ class camelot {
         return r >= 0 && c >= 0 && r < R && c < C;
     }
 
-    static class State {
-        List<Integer> moves;
-        List<Peice> peices;
+    static boolean onGrid(Point p) {
+        return onGrid(p.r,p.c);
+    }
 
-        State(List<Peice> peices, List<Integer> moves) {
-            this.peices = peices;
-            this.moves = moves;
-        }
+    static class State {
+        List<Point> moves;
+        Map<Point,Peice> board;
 
         State(List<Peice> peices) {
-            this(peices, new List<Integer>());
+            this.moves = new ArrayList<Point>();
+            this.board = new HashMap<Point, Peice>();
+            for(Peice peice: peices) {
+                this.board.put(peice.p, peice);
+            }
+        }
+
+        State(List<Point> moves, Map<Point,Peice> board) {
+            this.moves = moves;
+            this.board = board;
         }
 
         @Override
@@ -76,26 +91,101 @@ class camelot {
             return this.moves.hashCode();
         }
 
-        @Override
         public boolean equals(State s) {
             return s.hashCode() == this.hashCode();
         }
 
-        List<Integer> next() {
-            for(Peice p: peices) {
-                if()
+        List<State> next() {
+            List<State> states = new ArrayList<State>();
+            for(Map.Entry<Point,Peice> entry: this.board.entrySet()) {
+                states.addAll(this.move(entry.getValue()));
             }
+            return states;
+        }
+
+        List<State> move(Peice peice) {
+            List<State> states = new ArrayList<State>();
+            for(Point p: peice.moveSet()) {
+                if(onGrid(p)) {
+                    State s = this.copy();
+
+                    if(s.board.keySet().contains(p) && peice.type != 'k') {
+                        s.board.remove(peice.p);
+                    } else {
+                        s.board.remove(peice.p);
+                        if(s.board.get(p).type != 'k') {
+                            s.board.put(p, peice.copy(p));
+                        };
+                        s.moves.add(peice.p);
+                        s.moves.add(p);
+                        states.add(s);
+                    }
+                }
+            }
+
+            return states;
+        }
+
+        State copy() {
+            return new State(this.moves, this.board);
         }
     }
 
     static class Peice {
-        int r,c;
+        Point p;
         char type; // 'h' or 'k'
 
         Peice(int r, int c, char type) {
+            this.p = new Point(r,c);
+            this.type = type;
+        }
+
+        List<Point> moveSet() {
+            List<Point> ret = new ArrayList<Point>();
+            switch(this.type) {
+                case 'h':
+                    ret.add(this.p.moveCopy(1,2));
+                    ret.add(this.p.moveCopy(-1,2));
+                    ret.add(this.p.moveCopy(1,-2));
+                    ret.add(this.p.moveCopy(-1,-2));
+                    ret.add(this.p.moveCopy(2,1));
+                    ret.add(this.p.moveCopy(-2,1));
+                    ret.add(this.p.moveCopy(2,-1));
+                    ret.add(this.p.moveCopy(-2,-1));
+                    break;
+                case 'k':
+                    ret.add(this.p.moveCopy(1,0));
+                    ret.add(this.p.moveCopy(-1,0));
+                    ret.add(this.p.moveCopy(0,1));
+                    ret.add(this.p.moveCopy(0,-1));
+                    ret.add(this.p.moveCopy(1,1));
+                    ret.add(this.p.moveCopy(-1,1));
+                    ret.add(this.p.moveCopy(1,-1));
+                    ret.add(this.p.moveCopy(-1,-1));
+                    break;
+            }
+
+            return ret;
+        }
+
+        Peice copy(Point p) {
+            return new Peice(p.r, p.c, this.type);
+        }
+        Peice copy() {
+            return new Peice(this.p.r, this.p.c, this.type);
+        }
+    }
+
+    static class Point {
+        int r,c;
+
+        Point(int r, int c) {
             this.r = r;
             this.c = c;
-            this.type = type;
+        }
+
+        Point moveCopy(int addr,int addc) {
+            return new Point(this.r + addr, this.c + addc);
         }
     }
 }
