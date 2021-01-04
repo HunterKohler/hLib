07d0d36b053a726eecb50b89edce21f75a1aea1e
John H. Kohler <jhunterkohler@gmail.com>
2021-01-04 12:32:01 -0500

    working

---
+++ library/c++/nd_vector.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template <class T, size_t D>
+struct nd_vector : public std::vector<nd_vector<T, D - 1> > {
+    const size_t dimension = D;
+    static_assert(D >= 1, "Dimension must be 1 or greater.");
+
+    template <class ... U>
+    nd_vector(int n = 0, U ... args)
+        : std::vector<nd_vector<T, D - 1>>(n, nd_vector<T, D - 1>(args...)){ }
+};
+
+template <class T>
+struct nd_vector<T, 1> : public std::vector<T> {
+    const size_t dimension = 1;
+    nd_vector(int n = 0, const T& v = T()) : std::vector<T>(n, v){ }
+};
+
+int main(){
+    nd_vector<int,5> v;
+
+    cout << v[0][1][2][3][4] << endl;
+    z
+}
