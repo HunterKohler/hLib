8ecd61a467d3233842ff82ecbc9b22b814d654bb
John H. Kohler <jhunterkohler@gmail.com>
2021-03-13 16:03:16 -0500

    working

--- overthewire/natas/src.php
+++ overthewire/natas/src.php
@@ -1,54 +1,73 @@
-<html>
-<head>
-<!-- This stuff in the header has nothing to do with the level -->
-<link rel="stylesheet" type="text/css" href="http://natas.labs.overthewire.org/css/level.css">
-<link rel="stylesheet" href="http://natas.labs.overthewire.org/css/jquery-ui.css" />
-<link rel="stylesheet" href="http://natas.labs.overthewire.org/css/wechall.css" />
-<script src="http://natas.labs.overthewire.org/js/jquery-1.9.1.js"></script>
-<script src="http://natas.labs.overthewire.org/js/jquery-ui.js"></script>
-<script src=http://natas.labs.overthewire.org/js/wechall-data.js></script><script src="http://natas.labs.overthewire.org/js/wechall.js"></script>
-<script>var wechallinfo = { "level": "natas17", "pass": "<censored>" };</script></head>
-<body>
-<h1>natas17</h1>
-<div id="content">
-<?
-
-/*
-CREATE TABLE `users` (
-  `username` varchar(64) DEFAULT NULL,
-  `password` varchar(64) DEFAULT NULL
-);
-*/
-
-if(array_key_exists("username", $_REQUEST)) {
-    $link = mysql_connect('localhost', 'natas17', '<censored>');
-    mysql_select_db('natas17', $link);
-
-    $query = "SELECT * from users where username=\"".$_REQUEST["username"]."\"";
-    if(array_key_exists("debug", $_GET)) {
-        echo "Executing query: $query<br>";
+<?php
+    function setLanguage(){
+        /* language setup */
+        if(array_key_exists("lang",$_REQUEST))
+            if(safeinclude("language/" . $_REQUEST["lang"] ))
+                return 1;
+        safeinclude("language/en");
     }
 
-    $res = mysql_query($query, $link);
-    if($res) {
-    if(mysql_num_rows($res) > 0) {
-        //echo "This user exists.<br>";
-    } else {
-        //echo "This user doesn't exist.<br>";
-    }
-    } else {
-        //echo "Error in query.<br>";
+    function safeinclude($filename){
+        // check for directory traversal
+        if(strstr($filename,"../")){
+            logRequest("Directory traversal attempt! fixing request.");
+            $filename=str_replace("../","",$filename);
+        }
+        // dont let ppl steal our passwords
+        if(strstr($filename,"natas_webpass")){
+            logRequest("Illegal file access detected! Aborting!");
+            exit(-1);
+        }
+        // add more checks...
+
+        if (file_exists($filename)) {
+            include($filename);
+            return 1;
+        }
+        return 0;
     }
 
-    mysql_close($link);
-} else {
+    function listFiles($path){
+        $listoffiles=array();
+        if ($handle = opendir($path))
+            while (false !== ($file = readdir($handle)))
+                if ($file != "." && $file != "..")
+                    $listoffiles[]=$file;
+
+        closedir($handle);
+        return $listoffiles;
+    }
+
+    function logRequest($message){
+        $log="[". date("d.m.Y H::i:s",time()) ."]";
+        $log=$log . " " . $_SERVER['HTTP_USER_AGENT'];
+        $log=$log . " \"" . $message ."\"\n";
+        $fd=fopen("/var/www/natas/natas25/logs/natas25_" . session_id() .".log","a");
+        fwrite($fd,$log);
+        fclose($fd);
+    }
 ?>
 
-<form action="index.php" method="POST">
-Username: <input name="username"><br>
-<input type="submit" value="Check existence" />
+<h1>natas25</h1>
+<div id="content">
+<div align="right">
+<form>
+<select name='lang' onchange='this.form.submit()'>
+<option>language</option>
+<?php foreach(listFiles("language/") as $f) echo "<option>$f</option>"; ?>
+</select>
 </form>
-<? } ?>
+</div>
+
+<?php
+    session_start();
+    setLanguage();
+
+    echo "<h2>$__GREETING</h2>";
+    echo "<p align=\"justify\">$__MSG";
+    echo "<div align=\"right\"><h6>$__FOOTER</h6><div>";
+?>
+<p>
 <div id="viewsource"><a href="index-source.html">View sourcecode</a></div>
 </div>
 </body>
